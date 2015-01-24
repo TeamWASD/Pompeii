14896bd36c07246dcf1ce47ea3556dd3dc01bcd5
Godavarism <godavarism@gmail.com>
2015-01-24 16:37:52 -0700

    Circular Exclusion Zone

--- Assets/Scripts/TerrainGen.cs
+++ Assets/Scripts/TerrainGen.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class TerrainGen : MonoBehaviour
@@ -11,8 +12,15 @@ public class TerrainGen : MonoBehaviour
 
 	void Start () {
 		for (int i=0; i < RockCount; ++i) {
-			Vector3 pos = new Vector3(Random.value * 100.0f - 50.0f, Random.value * 100.0f - 50.0f, 0);
-			Instantiate(Rock, pos, Quaternion.identity);
+			Vector3 pos = new Vector3(UnityEngine.Random.value * 100.0f - 50.0f, UnityEngine.Random.value * 100.0f - 50.0f, 0);
+            if (Math.Sqrt((Math.Abs(pos.x) * Math.Abs(pos.x)) + (Math.Abs(pos.y) * Math.Abs(pos.y))) > 15)
+            {
+			    Instantiate(Rock, pos, Quaternion.identity);
+            }
+            else
+            {
+                --i;
+            }
 		}
 
 	}
