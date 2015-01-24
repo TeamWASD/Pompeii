6f7077e56f6446335cf6ed3c68e580735029453e
LRFLEW <lrflew@aol.com>
2015-01-24 12:11:17 -0700

    Got a Smart Camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,27 +10,26 @@ public class CameraFollow : MonoBehaviour {
 
 	private Transform target;
 	private Transform follow;
-	private SpriteRenderer spriteBounds;
 
 	// Use this for initialization
 	void Start () {
 		float vertExtent = Camera.main.camera.orthographicSize;  
 		float horzExtent = vertExtent * Screen.width / Screen.height;
 
-		target = GameObject.Find("Player").transform;
-		follow = GameObject.Find ("Main Camera").transform;
+		target = GameObject.Find("Main Camera").transform;
+		follow = GameObject.Find("Player").transform;
 
-		spriteBounds = GameObject.Find("Background").GetComponentInChildren<SpriteRenderer>();
+		Renderer background = GameObject.Find("Background").renderer;
 
-		leftBound = (float)(horzExtent - spriteBounds.sprite.bounds.size.x / 2.0f);
-		rightBound = (float)(spriteBounds.sprite.bounds.size.x / 2.0f - horzExtent);
-		bottomBound = (float)(vertExtent - spriteBounds.sprite.bounds.size.y / 2.0f);
-		topBound = (float)(spriteBounds.sprite.bounds.size.y  / 2.0f - vertExtent);
+		leftBound = (float)(background.bounds.min.x + horzExtent);
+		rightBound = (float)(background.bounds.max.x - horzExtent);
+		bottomBound = (float)(background.bounds.min.x + vertExtent);
+		topBound = (float)(background.bounds.max.x - vertExtent);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var pos = new Vector3(follow.position.x, follow.position.y, 0);
+		var pos = new Vector3(follow.position.x, follow.position.y, -10);
 		pos.x = Mathf.Clamp(pos.x, leftBound, rightBound);
 		pos.y = Mathf.Clamp(pos.y, bottomBound, topBound);
 		target.position = pos;
