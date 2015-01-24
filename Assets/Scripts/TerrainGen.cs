d5bf2eaa3ce2ee83430a4286946b04186f1cc996
Keiwaiou <kristindouglass@vlan814-052-224.pioneernet.du.edu>
2015-01-24 13:52:06 -0700

    changed, only 1 start now

--- Assets/Scripts/TerrainGen.cs
+++ Assets/Scripts/TerrainGen.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TerrainGen : MonoBehaviour {
+public class TerrainGen : MonoBehaviour
+{
 
 	public int RockCount = 100;
 	public Transform Rock;
 
 	// Use this for initialization
+
 	void Start () {
 		for (int i=0; i < RockCount; ++i) {
 			Vector3 pos = new Vector3(Random.Range(-50,50), Random.Range (-50,50), 0);
 			Instantiate(Rock, pos, Quaternion.identity);
 		}
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 	
 	}
 }
