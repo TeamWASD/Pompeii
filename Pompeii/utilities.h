06d03594419c9079b82c4794d3b363ec642bacee
LRFLEW <lrflew@aol.com>
2015-01-23 21:35:28 -0700

    More Fixes
    Wooh

--- Pompeii/utilities.h
+++ Pompeii/utilities.h
@@ -6,12 +6,16 @@
 //  Copyright (c) 2014 LRFLEW. All rights reserved.
 //
 
-#ifndef __DinoDodge__utilities__
-#define __DinoDodge__utilities__
+#ifndef __Pompeii__utilities__
+#define __Pompeii__utilities__
 
+#ifdef SDL_STATIC_IMPORT
 #include <SDL2/SDL.h>
+#else
+#include "SDL.h"
+#endif
 
 void SDL_Log(SDL_LogPriority priority, const char* fmt, ...);
 void logAndCrashSDL(const char* error);
 
-#endif /* defined(__DinoDodge__utilities__) */
+#endif /* defined(__Pompeii__utilities__) */
