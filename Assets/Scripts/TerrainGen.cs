d0965ee9f452bdd54cbd7c0422e9dba3f319420f
LRFLEW <lrflew@aol.com>
2015-01-24 16:40:44 -0700

    Code Simplification

--- Assets/Scripts/TerrainGen.cs
+++ Assets/Scripts/TerrainGen.cs
@@ -12,15 +12,11 @@ public class TerrainGen : MonoBehaviour
 
 	void Start () {
 		for (int i=0; i < RockCount; ++i) {
-			Vector3 pos = new Vector3(UnityEngine.Random.value * 100.0f - 50.0f, UnityEngine.Random.value * 100.0f - 50.0f, 0);
-            if (Math.Sqrt((Math.Abs(pos.x) * Math.Abs(pos.x)) + (Math.Abs(pos.y) * Math.Abs(pos.y))) > 15)
-            {
-			    Instantiate(Rock, pos, Quaternion.identity);
-            }
-            else
-            {
-                --i;
-            }
+			Vector3 pos;
+			do {
+				pos = new Vector3(UnityEngine.Random.value * 100.0f - 50.0f, UnityEngine.Random.value * 100.0f - 50.0f, 0);   
+			} while (Math.Sqrt((Math.Abs(pos.x) * Math.Abs(pos.x)) + (Math.Abs(pos.y) * Math.Abs(pos.y))) < 15);
+			Instantiate(Rock, pos, Quaternion.identity);
 		}
 
 	}
