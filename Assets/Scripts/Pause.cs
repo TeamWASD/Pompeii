b5087534e39140f95361d10c1273df0ed1a4fdce
LRFLEW <lrflew@aol.com>
2015-01-25 01:29:32 -0700

    Pause Menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	private float _timeScale = 1.0f;
+	private bool isPaused = false;
+	private GameObject pause;
+
+	// Use this for initialization
+	void Start () {
+		pause = GameObject.FindWithTag ("Pause");
+		pause.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			TogglePause();
+		}
+	}
+
+	public void TogglePause() {
+		if (isPaused)
+			UnPauseGame ();
+		else
+			PauseGame ();
+	}
+
+	public void PauseGame() {
+		_timeScale = Time.timeScale;
+		Time.timeScale = 0.0f;
+		pause.SetActive (true);
+		isPaused = true;
+	}
+
+	public void UnPauseGame() {
+		Time.timeScale = _timeScale;
+		pause.SetActive (false);
+		isPaused = false;
+	}
+
+}
---
+++ Assets/Scripts/RemotePause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemotePause : MonoBehaviour {
+
+	public void TogglePause() {
+		GameObject.FindWithTag ("Player").GetComponent<Pause> ().TogglePause ();
+	}
+	
+	public void PauseGame() {
+		GameObject.FindWithTag ("Player").GetComponent<Pause> ().PauseGame ();
+	}
+	
+	public void UnPauseGame() {
+		GameObject.FindWithTag ("Player").GetComponent<Pause> ().UnPauseGame ();
+	}
+}
