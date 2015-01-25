61e6388c931bc27683493e60c36b3c1f24c25040
Godavarism <godavarism@gmail.com>
2015-01-25 00:08:28 -0700

    Network stuff which may or may not do anything

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -15,23 +15,21 @@ public class MoveScript : MonoBehaviour
 	// 2 - Store the movement
 	private Vector2 movement;
 	
-	void Update()
-	{
-		// 3 - Retrieve axis information
-		float inputX = Input.GetAxis("Horizontal");
-		float inputY = Input.GetAxis("Vertical");
-		
-		// 4 - Movement per direction
-		movement = new Vector2(
-			speed.x * inputX,
-			speed.y * inputY);
-		
-	}
-	
-	void FixedUpdate()
-	{
-		// 5 - Move the game object
-		rigidbody2D.velocity = movement;
-	}
+	void Update() 
+    {
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+        // 4 - Movement per direction
+        movement = new Vector2(
+        speed.x * inputX,
+        speed.y * inputY);
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Move the game object
+        rigidbody2D.velocity = movement;
+    }
 	
 }
