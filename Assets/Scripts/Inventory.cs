276f44213166b04a95ff475062807d78d7c31bd2
Godavarism <godavarism@gmail.com>
2015-01-24 17:20:09 -0700

    Workbench Interaction

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,20 +6,39 @@ public class Inventory : MonoBehaviour {
 	private int rockCount = 0;
 	public int RockCount
 	{
-		get{ return rockCount;}
+		get { return rockCount; }
 		set {
 			if (rockCount < value) {
 				for(; rockCount < value; ++rockCount) {
 					GameObject.Find("Star" + (1 + rockCount)).renderer.enabled = true;
 				}
-			} else {
+			}
+            else {
 				for(; rockCount > value; --rockCount) {
 					GameObject.Find("Star" + (rockCount)).renderer.enabled = false;
 				}
 			}
 		}
 	}
-
+    
+    private int brickCount = 0;
+    public int BrickCount
+    {
+        get { return brickCount; }
+        set {
+            if (brickCount < value) {
+                for (; brickCount < value; ++brickCount) {
+                    GameObject.Find("Brick" + (1 + brickCount)).renderer.enabled = true;
+                }
+            }
+            else {
+                for (; brickCount > value; --brickCount) {
+                    GameObject.Find("Brick" + (brickCount)).renderer.enabled = false;
+                }
+            }
+        }
+    }
+    
 	// Use this for initialization
 	void Start () {
 	
---
+++ Assets/Scripts/Workable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Workable : MonoBehaviour {
+
+    private bool collision = false;
+    private Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = GameObject.FindWithTag("Player").GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (collision && Input.GetKeyDown("space") && inventory.RockCount > 0)
+        {
+            inventory.RockCount--;
+            inventory.BrickCount++;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.gameObject.tag.Equals("Player"))
+        {
+            collision = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        collision = false;
+    }
+}
