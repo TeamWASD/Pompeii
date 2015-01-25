41b472ac13a32802e82ee94b8aba9f4227818afa
Keiwaiou <kristindouglass@vlan807-025-016.pioneernet.du.edu>
2015-01-24 18:24:09 -0700

    lava prefab now

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerStatus : MonoBehaviour {
 
+	private bool collision = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +12,17 @@ public class PlayerStatus : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		if (otherCollider.gameObject.tag.Equals("Player"))
+		{
+			collision = true;
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D otherCollider)
+	{
+		collision = false;
 	}
 }
