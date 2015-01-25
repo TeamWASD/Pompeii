0ef8b6cbc4b642b2ec955348c1ccde8774cfdb02
Keiwaiou <kristindouglass@vlan807-025-016.pioneernet.du.edu>
2015-01-24 20:02:11 -0700

    lava growing too fast

---
+++ Assets/Scripts/LavaScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaScript : MonoBehaviour {
+
+	public Vector2 startSize, endSize;
+	private float t = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		transform.localScale = Vector2.Lerp (startSize, endSize, t += 0.01f);
+	}
+}
